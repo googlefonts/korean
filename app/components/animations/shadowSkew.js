a736c4a947954fdebc783f688f4ca9a21d6f9b37
Wonyoung So <wso@mit.edu>
2018-02-05 12:02:12 +0800

    * Skeching Shadowskew.

---
+++ app/components/animations/shadowSkew.js
@@ -0,0 +1,12 @@
+// var circlePath1 = new Path.Circle(new Point(350, 350), 125);
+// var circlePath2 = new Path.Circle(new Point(550, 350), 125);
+// var circlePath3 = new Path.Circle(new Point(750, 350), 125);
+// circlePath1.strokeColor = 'black';
+// circlePath2.strokeColor = 'black';
+// circlePath3.strokeColor = 'black';
+
+// var g = new Group([circlePath1, circlePath2, circlePath3]);
+// var d = g.clone();
+// d.shear(0.0, -0.5, new Point(550, 350));
+// g.shear(0.0, 0.5, new Point(550, 350));
+// // circlePath.
