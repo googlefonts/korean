6936b2f28930ee9f4d03ec2da11a0dbba0c05b6a
Wonyoung So <mail@wonyoung.so>
2018-02-12 23:26:44 +0800

    wavyBaseline WIP

--- app/components/animations/wavyBaseline.js
+++ app/components/animations/wavyBaseline.js
@@ -1,22 +1,50 @@
 import paper from 'paper';
 import { convertBgMode } from '../../utils';
 
+const interpolateCompoundPath = (path) => {
+  
+  var interpolatedPath = new paper.CompoundPath();
+  
+  for (var i = 0; i < path.children.length; i++) {
+      var childIntPath = new paper.Path();
+      var child = path.children[i];
+      var amount = 100;
+      var length = child.length;
+      
+      for (var j = 0; j < amount + 1; j++) {
+          var offset = j / amount * length;
+          var point = child.getPointAt(offset);
+          // var circle = new Path.Circle({
+          //     center: point,
+          //     radius: 2,
+          //     fillColor: 'red'
+          // });
+          childIntPath.add(point);
+      }
+      childIntPath.closed = true;
+      interpolatedPath.addChild(childIntPath);
+  }  
+  interpolatedPath.fullySelected = true;
+  return interpolatedPath;
+};
+
 export const wavyBaseline = {
   attach: (_this, backgroundMode) => {
     // console.log(fontAn);
     _this.wavyBaseline = {
       glyphs: []
     };
-    
+
+   
     _this.project.activate();
 
     
     _.each(_this.glyphs, (glyph, i) => {
 
-      var _g = glyph.clone();
+      // var _g = glyph.clone();
+      var _g = interpolateCompoundPath(glyph);
       glyph.visible = false;
       
-      _g.flatten(1);
       _g.fillColor = convertBgMode(backgroundMode, "b");
       _g.strokeColor = convertBgMode(backgroundMode, "f");
       // g.fillColor = convertBgMode(backgroundMode, "b");
@@ -24,10 +52,12 @@ export const wavyBaseline = {
       _this.wavyBaseline.glyphs.push(_g);
     });
 
+    // _this.wavyBaseline.dx = (Math.PI / ) * 50;
+
 
     _this.view.onFrame = (e) => {
 
-    var i = 0;
+      var i = 0;
       _.each(_this.wavyBaseline.glyphs, g => {
         
         _.each(g.children, child => {
@@ -36,17 +66,13 @@ export const wavyBaseline = {
 
             let p = seg.point.clone();
 
-            if (j % 2 == 0) {
-              seg.point = new paper.Point(p.x, p.y + Math.sin(e.time + i) * 0.25);  
-            } else {
-              seg.point = new paper.Point(p.x, p.y + Math.cos(e.time + i) * 0.25);
-            }
-            
-            i+= 1;
+
+
           });
 
-        });
 
+        });
+        
       });  
 
 
@@ -89,3 +115,47 @@ export const wavyBaseline = {
 
   }
 };
+
+// var width, height, center;
+// var points = 30;
+// var smooth = false;
+// var path = new Path();
+// var theta = 0;
+// var dx = 0;
+
+  
+// path.fillColor = 'black';
+// initializePath();
+
+// function initializePath() {
+//   center = view.center;
+//   width = view.size.width;
+//   height = view.size.height / 2;
+//   path.segments = [];
+//   dx = (Math.PI * 2 / view.size.width) * 50;
+//   path.add(view.bounds.bottomLeft);
+//   for (var i = 1; i < points; i++) {
+//     var point = new Point(width / points * i, center.y);
+//     path.add(point);
+//   }
+//   path.add(view.bounds.bottomRight);
+//   path.fullySelected = true;
+// }
+
+// function onFrame(event) {
+//     theta += 0.02;
+
+//   var x = theta;
+//   for (var i = 1; i < points; i++) {
+//     var yPos = Math.sin(x) * 50 + height;
+//     path.segments[i].point.y = yPos;
+//     x += dx;
+//   }
+//   if (smooth)
+//     path.smooth({ type: 'continuous' });
+// }
+
+// // Reposition the path whenever the window is resized:
+// function onResize(event) {
+//   initializePath();
+// }
