daaa3f0f9b4bf5f9b35b99291515ba00ca030275
Wonyoung So <mail@wonyoung.so>
2018-02-27 18:12:29 +0900

    Try with scrollingelement

--- app/components/App.js
+++ app/components/App.js
@@ -73,8 +73,8 @@ class App extends Component {
     }
     let categoryPosTop = document.querySelector(`a[name=category-${currentCategory}]`).offsetTop;
     let offset = 20;
-  
-    TweenMax.to((document.body || document.documentElement || document.body.parentNode), 1, { ease: Power3.easeInOut, scrollTop: categoryPosTop - collapsedHeaderHeight - offset - headerHeight });
+
+    TweenMax.to((document.scrollingElement || document.documentElement), 1, { ease: Power3.easeInOut, scrollTop: categoryPosTop - collapsedHeaderHeight - offset - headerHeight });
   }
 
   handleScroll(e){
